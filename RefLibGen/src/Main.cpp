d6084d76ca4dc78b8cc6fc99e793ae065f6d2ea0
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2022-06-14 02:48:36 -0400

    Tested antlr

--- RefLibGen/src/Main.cpp
+++ RefLibGen/src/Main.cpp
@@ -2,9 +2,24 @@
 
 #include "CPP14Lexer.h"
 #include "CPP14Parser.h"
+#include "CPP14ParserBaseListener.h"
 #include "antlr4-runtime.h"
 
 using namespace antlr4;
+
+class FunctionDefinitionLisener : public CPP14ParserBaseListener
+{
+public:
+	FunctionDefinitionLisener(Parser& parser) : m_Parser(&parser) {}
+
+	void enterEveryRule(antlr4::ParserRuleContext* context) override 
+	{ 
+		std::cout << context->getStart()->getText() << "\n";
+	}
+
+private:
+	Parser* m_Parser;
+};
 
 int main()
 {
@@ -15,6 +30,10 @@ int main()
 	CommonTokenStream tokens(&lexer);
 	CPP14Parser parser(&tokens);
 
-	tree::ParseTree* tree = parser.translationUnit();
-	std::cout << tree->toStringTree(true);
+	/*tree::ParseTree* tree = parser.translationUnit();
+	std::cout << tree->toStringTree(true);*/
+
+	tree::ParseTree* tree = parser.functionDefinition();
+	FunctionDefinitionLisener listener = FunctionDefinitionLisener(parser);
+	tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
 }
